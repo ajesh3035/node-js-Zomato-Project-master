2b06c946a2a33489f376c1675eafe1fae68ba427
ajesh3035 <158299515+ajesh3035@users.noreply.github.com>
2025-12-31 20:04:09 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -84,7 +84,7 @@ const Footer = () => {
         <p>
           By continuing past this page, you agree to our Terms of Service,
           Cookie Policy, Privacy Policy and Content Policies. All trademarks are
-          properties of their respective owners. 2008-2022 © Zomato™ Ltd. All
+          properties of their respective owners. 2025-2026 © Zomato™ Ltd. All
           rights reserved.
         </p>
       </div>
